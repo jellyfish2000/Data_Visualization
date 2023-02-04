09df2b584f6a3ddf66a0552ea2a2a76bf43f8758
Preethi <47339289+pre6@users.noreply.github.com>
2023-02-03 23:26:22 -0500

    Create SQL_Queries.sql

---
+++ SQL_Queries.sql
@@ -0,0 +1,30 @@
+SELECT 'SUM' Date, COUNT(Date)
+FROM Thermostat
+WHERE [System Mode] = 'heatStage1On'
+AND Date BETWEEN '2022-06-23' AND '2022-09-01'
+
+UNION ALL
+
+
+SELECT 'SUM' Date, COUNT(Date)
+FROM Thermostat
+WHERE [System Mode] = 'heatStage1On'
+AND Date BETWEEN '2022-09-02' AND '2022-10-05'
+
+UNION ALL
+
+
+SELECT 'SUM' Date, COUNT(Date)
+FROM Thermostat
+WHERE [System Mode] = 'heatStage1On'
+AND Date BETWEEN '2022-10-06' AND '2022-11-01'
+
+UNION ALL
+
+SELECT 'SUM' Date, COUNT(Date)
+FROM Thermostat
+WHERE [System Mode] = 'heatStage1On'
+AND Date BETWEEN '2022-11-02' AND '2022-12-02';
+
+
+
